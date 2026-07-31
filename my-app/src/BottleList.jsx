e663cda69819b9b88a8a367274779d0c4aae5f22
Munkhjargal <munkhjargal.intern@andsystems.tech>
2026-07-31 15:55:55 +0800

    update bottle button

--- my-app/src/BottleList.jsx
+++ my-app/src/BottleList.jsx
@@ -1,11 +1,30 @@
 import {useEffect, useState} from "react";
 import {Button, Group, Table, TextInput, Select, TabsList} from "@mantine/core";
-import {Route, Routes, Link} from "react-router-dom";
 import {hasLength, useForm} from "@mantine/form";
 import Modal from "./Components/Modal.jsx";
 
 function BottleList(){
     const [openModal, setOpenModal] = useState(false);
+    const [openModalTwo, setOpenModalTwo] = useState(false);
+    const [updatedBottle, setUpdatedBottle] = useState(null);
+
+    const updateForm = useForm({
+        mode: 'uncontrolled',
+        initialValues: {
+            vendorId: "",
+            brand: "",
+            capacity: "",
+            barcode: ""
+        },
+
+        validate: {
+            vendorId: hasLength({ min: 1, max: 10 }, 'Vendor Id must be 1-10 characters long'),
+            brand: hasLength({ min: 1, max: 10 }, 'Brand must be 1-10 characters long'),
+            capacity: (value) => {Number(value) > 0 ? null : "Capacity must be greater than 0"},
+            barcode: hasLength({ min: 2, max: 100 }, 'Barcode must be 2-10 characters long'),
+        }
+    })
+
     const handleSubmit = (values) => {
         fetch("http://localhost:8080/water-bottles/",
             {
@@ -21,6 +40,18 @@ function BottleList(){
                 .catch(error => console.log(error));
     }
 
+    const handleUpdate = (values) => {
+        fetch(`http://localhost:8080/water-bottles/${updatedBottle.id}`,
+            {
+                method: "PUT",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(values)
+            })
+            .then(response => response.json())
+            .then(result => setBottleList(result.data))
+            .catch(error => console.log(error));
+    }
+
     const handleDelete = (id) => {
         fetch(`http://localhost:8080/water-bottles/${id}`,
             {
@@ -31,15 +62,6 @@ function BottleList(){
                 .catch(error => console.log(error));
     }
 
-    const handleUpdate = (id) => {
-        fetch(`http://localhost:8080/water-bottles/${id}`,
-            {
-                method: "UPDATE",
-            })
-            .then(response => response.json())
-            .then(result => setBottleList(result.data))
-            .catch(error => console.log(error));
-    }
     const [bottleList, setBottleList] = useState([])
     useEffect(() => {
         fetch(`http://localhost:8080/water-bottles?page=0&pageSize=100`)
@@ -53,16 +75,34 @@ function BottleList(){
             .then(response => response.json())
             .then(result => setVendorList(result.data))
     }, [])
+    const openUpdateModal = (bottle) => {
+        setUpdatedBottle(bottle);
+
+        updateForm.setValues({
+            vendorId: bottle.vendorId.toString(),
+            brand: bottle.brand,
+            capacity: bottle.capacity,
+            barcode: bottle.barcode,
+        });
+        setOpenModalTwo(true);
+    }
     const rows = bottleList.map((bottle) => (
         <Table.Tr key={bottle.id}>
-            <Table.Td><Button onClick={() => handleUpdate(bottle.id)}>update</Button></Table.Td>
+            <Table.Td>
+                <Button onClick={() => openUpdateModal(bottle)}>
+                    Update
+                </Button>
+            </Table.Td>
             <Table.Td>{bottle.id}</Table.Td>
             <Table.Td>{bottle.brand}</Table.Td>
             <Table.Td>{bottle.capacity}</Table.Td>
             <Table.Td>{bottle.barcode}</Table.Td>
             <Table.Td>{bottle.vendorId}</Table.Td>
-            <Table.Td><Button onClick={() => handleDelete(bottle.id)}>delete</Button></Table.Td>
-
+            <Table.Td>
+                <Button onClick={() => handleDelete(bottle.id)}>
+                    Delete
+                </Button>
+            </Table.Td>
         </Table.Tr>
     ));
 
@@ -91,7 +131,7 @@ function BottleList(){
                     setOpenModal(true);
                 }}
             >
-                Create Bottle
+                Create
             </Button>
             {openModal &&
                 <Modal
@@ -139,6 +179,48 @@ function BottleList(){
                     </form>
                 </Modal>
             }
+
+            {openModalTwo && updatedBottle && (
+                <Modal
+                    closeModal={setOpenModalTwo}
+                    title="Update Bottle"
+                >
+                    <form onSubmit={updateForm.onSubmit(handleUpdate)}>
+                        <Select
+                            label="vendorId"
+                            data={vendorList.map(vendor => ({
+                                value: vendor.id.toString(),
+                                label: vendor.name,
+                            }))}
+                            {...updateForm.getInputProps("vendorId")}
+                        />
+                        <TextInput
+                            label="brand"
+                            withAsterisk
+                            mt="md"
+                            key={updateForm.key('brand')}
+                            {...updateForm.getInputProps('brand')}
+                        />
+                        <TextInput
+                            label="capacity"
+                            withAsterisk
+                            mt="md"
+                            key={updateForm.key('capacity')}
+                            {...updateForm.getInputProps('capacity')}
+                        />
+                        <TextInput
+                            label="barcode"
+                            withAsterisk
+                            mt="md"
+                            key={updateForm.key('barcode')}
+                            {...updateForm.getInputProps('barcode')}
+                        />
+                        <Group justify="flex-end" mt="md">
+                            <Button type="submit">Update Bottle</Button>
+                        </Group>
+                    </form>
+                </Modal>
+            )}
             <Table>
                 <Table.Thead>
                     <Table.Tr>
